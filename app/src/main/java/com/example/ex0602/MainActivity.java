26e3b3016aa7fc5b2b84f179f39859f7bad63062
sh7478 <sh7478@bs.amalnet.k12.il>
2024-12-27 18:31:57 +0200

    first step

--- app/src/main/java/com/example/ex0602/MainActivity.java
+++ app/src/main/java/com/example/ex0602/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex0602;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.mainbutton);
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                btn.setText("Oh, yea, I've been clicked!");
+            }
+        });
     }
+
+
+
 }
